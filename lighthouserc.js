2319f87e8aae59338d6bb63b7e8ea6ae8c91b77d
pkmkumar3 <pkmkumar3@gmail.com>
2022-09-19 23:50:04 -0700

    edit lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,23 +1,9 @@
-{
-  "ci": {
-    "collect": {
-      "chromePath": false,
-      "numberOfRuns": 1,
-      "puppeteerScript": "./lighthouse/pre-collect.js",
-      "url": ""
+module.exports = {
+  ci: {
+    upload: {
+      target: 'temporary-public-storage',
+      token: "8d6a1879-7e63-48ad-8b35-b4799761d31c",
+      serverBaseUrl: "http://20.237.121.213/",
     },
-    "assert": {
-      "assertions": {
-        "categories:performance": ["error", { "minScore": 0.90 }],
-        "categories:accessibility": ["error", { "minScore": 0.90 }],
-        "categories:best-practices": ["error", { "minScore": 0.90 }],
-        "categories:seo": ["error", { "minScore": 0.90 }]
-      }
-    },
-    "upload": {
-      "target": "lhci",
-      "token": "8d6a1879-7e63-48ad-8b35-b4799761d31c",
-      "serverBaseUrl": "http://20.237.121.213/"
-    }
-  }
-}
+  },
+};
