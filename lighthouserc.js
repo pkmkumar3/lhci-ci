c285ab18cebbf5cf0bdcb1bb721418bcde177d1a
pkmkumar3 <pkmkumar3@gmail.com>
2022-09-19 21:49:48 -0700

    updated lighthouserc file

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,9 +2,13 @@ module.exports = {
     ci: {
       collect: {
         url: ['http://localhost:8080'] /* Add configuration here */
+        "chromePath": false,
+        "numberOfRuns": 1,
       },
       upload: {
         target: 'temporary-public-storage', /* Add configuration here */
+        "token": "e0e319af-7d58-4f8c-9f75-e85856354ca0",
+        "serverBaseUrl": "http://20.237.121.213/"
       },
     },
   };
