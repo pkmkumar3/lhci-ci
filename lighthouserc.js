038110b0335fca86aa18e526a278f2d29a99987b
pkmkumar3 <pkmkumar3@gmail.com>
2022-09-19 21:38:53 -0700

    updated lighthouserc.js

---
+++ lighthouserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ci: {
+      collect: {
+        url: ['http://localhost:8080'] /* Add configuration here */
+      },
+      upload: {
+        target: 'temporary-public-storage', /* Add configuration here */
+      },
+    },
+  };
