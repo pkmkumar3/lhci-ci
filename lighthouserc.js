a6d136368314474a2d6281d64c81cf0b3a5bfd31
pkmkumar3 <pkmkumar3@gmail.com>
2022-09-20 00:17:18 -0700

    change in lighthouserc file

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,9 +1,10 @@
 module.exports = {
   ci: {
+    collect: {
+      staticDistDir: './',
+    },
     upload: {
       target: 'temporary-public-storage',
-      token: "8d6a1879-7e63-48ad-8b35-b4799761d31c",
-      serverBaseUrl: "http://20.237.121.213/",
     },
   },
 };
